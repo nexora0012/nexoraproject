c4211795dd75b67ae2f6d9d45a15fb8e308ecf3d
nexora0012 <yotene9328@barumart.com>
2026-07-24 19:58:11 +0530

    Add Fast2SMS error logging

--- backend/src/utils/sendSms.js
+++ backend/src/utils/sendSms.js
@@ -1,17 +1,36 @@
 const axios = require("axios");
 
 const sendOtpSms = async (mobile, otp) => {
-  const response = await axios.get("https://www.fast2sms.com/dev/bulkV2", {
-    params: {
-      authorization: process.env.FAST2SMS_API_KEY,
-      route: "otp",
-      variables_values: otp,
-      flash: 0,
-      numbers: mobile,
-    },
-  });
+  try {
+    console.log("Sending OTP to:", mobile);
 
-  return response.data;
+    const response = await axios.get(
+      "https://www.fast2sms.com/dev/bulkV2",
+      {
+        params: {
+          authorization: process.env.FAST2SMS_API_KEY,
+          route: "otp",
+          variables_values: otp,
+          flash: 0,
+          numbers: mobile,
+        },
+      }
+    );
+
+    console.log("Fast2SMS Response:", response.data);
+
+    return response.data;
+  } catch (error) {
+    console.error("Fast2SMS Error:");
+
+    if (error.response) {
+      console.error(error.response.data);
+    } else {
+      console.error(error.message);
+    }
+
+    throw error;
+  }
 };
 
 module.exports = sendOtpSms;
